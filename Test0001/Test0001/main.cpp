0e9a16473c369d954231f4535d5ea4f009f89239
takaura-f-klab <takaura-f@klab.com>
2013-11-11 14:43:18 +0900

    first commit

---
+++ Test0001/Test0001/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test0001
+//
+//  Created by takaura-f on 13/11/11.
+//  Copyright (c) 2013年 takaura-f. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
